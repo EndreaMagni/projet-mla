2939131b7ae7ae562cc736815d6208978ab37fa1
FerielBOUCHAKOUR <bouchakourferiel.25@gmail.com>
2023-12-13 17:59:16 +0100

    evaluation added to the training

--- src/Baseline/train.py
+++ src/Baseline/train.py
@@ -11,7 +11,7 @@ def make_batch(data,batch_size):
     batches = [pairs[i:i + batch_size] for i in range(0, len(pairs), batch_size)]
     return np.array(batches)
 
-def one_hot_encode_batch(batch,vocab_size,word_to_id_eng,word_to_id_fr):
+def index_batch(batch,word_to_id_eng,word_to_id_fr):
     input_batch = []
     output_batch = []
     for pair in batch: 
@@ -30,37 +30,75 @@ def Init_weights(model):
         else                    : nn.init.constant_(param.data, 0)
 
 
-def train(model,train_data,word_to_id_eng,word_to_id_fr,batch_size,vocab_size,learning_rate,epochs,device,print_every=1,):
-    
+          
+def train(model, train_data, val_data, word_to_id_eng, word_to_id_fr, batch_size, vocab_size, learning_rate, epochs, device,print_every):
+    model = model.to(device)
     model.apply(Init_weights)
-    
-    batches=make_batch(train_data,batch_size)
-    
-    # Initialize Adadelta optimizer with the given epsilon and rho values
     optimizer = torch.optim.Adadelta(model.parameters(), lr=learning_rate, rho=0.95, eps=1e-06)
-
     criterion = nn.NLLLoss(reduction="mean")
+
+    train_losses = []
+    val_losses = []
+    best_val_loss = float('inf')
+    best_model = None
+     
+    train_batches=make_batch(train_data, batch_size)
+    val_batches=make_batch(val_data, batch_size)
+    
     for epoch in range(epochs):
-        
-        for batch in batches:
-            input_batch,output_batch = one_hot_encode_batch(batch,len(word_to_id_eng),word_to_id_eng,word_to_id_fr)
+        attention_weights=[]
+        model.train()
+        total_loss = 0
+        for batch in train_batches:
+            input_batch, output_batch = index_batch(batch, len(word_to_id_eng), word_to_id_eng, word_to_id_fr)
+            input_batch, output_batch = input_batch.to(device), output_batch.to(device)
+
             optimizer.zero_grad()
-            output,attention_weights  = model(input_batch)
-            # output_onehot  = F.one_hot(output_batch, num_classes=len(word_to_id_fr)).float()
-            breakpoint()
+            output, attention_weights = model(input_batch)
             output = output.reshape(-1, vocab_size)
             output = F.log_softmax(output, dim=1)
             output_batch = output_batch.view(-1).long()
 
-            loss = criterion(output,output_batch)
-            breakpoint()
+            loss = criterion(output, output_batch)
             loss.backward()
             optimizer.step()
+            total_loss += loss.item()
+
+        avg_train_loss = total_loss / len(train_data)
+        train_losses.append(avg_train_loss)
+
+        # Validation
+        model.eval()
+        total_val_loss = 0
+        with torch.no_grad():
+            for batch in val_batches:
+                input_batch, output_batch =index_batch(batch, len(word_to_id_eng), word_to_id_eng, word_to_id_fr)
+                input_batch, output_batch = input_batch.to(device), output_batch.to(device)
+                output, _ = model(input_batch)
+                output = output.reshape(-1, vocab_size)
+                output = F.log_softmax(output, dim=1)
+                output_batch = output_batch.view(-1).long()
+
+                val_loss = criterion(output, output_batch)
+                total_val_loss += val_loss.item()
+
+        avg_val_loss = total_val_loss / len(val_data)
+        val_losses.append(avg_val_loss)
+
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            best_model = model.state_dict()
+            best_attention_weights=attention_weights
+
         if epoch % print_every == 0:
-            print('Epoch: ', epoch + 1, 'loss =', '{:.6f}'.format(loss))
-            
+            print(f'Epoch: {epoch + 1}, Training Loss: {avg_train_loss:.6f}, Validation Loss: {avg_val_loss:.6f}')
+
     print('Training Finished')
-          
+    np.save('train_losses.npy', np.array(train_losses))
+    np.save('val_losses.npy', np.array(val_losses))
+    torch.save(best_model, 'best_model.pth')
+    return best_attention_weights
+
           
 
      
