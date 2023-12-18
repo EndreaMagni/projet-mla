9da24f45f37936b5a397206c1f3e2749c7f342a3
LindaRahaoui <Linda.rahaoui16@gmail.com>
2023-12-18 01:40:49 +0100

    delted sharelink

--- src/Baseline/translation.py
+++ src/Baseline/translation.py
@@ -52,170 +52,233 @@ class BaselineTrainer:
             else:
                 nn.init.constant_(param.data, 0)
 
-    def train(self, model, train_data_loader,val_data_loader, device,
-              nohup = False, mode = "RNNEncDec",seq_len = cfg.sequence_length, 
-              load_model = "Nothing", chosed_optimizer = "adadelta", lr = 0) :
+def train(self, model, train_data_loader, val_data_loader, device,
+          nohup=False, mode="RNNEncDec", seq_len=cfg.sequence_length,
+          load_model="Nothing", chosed_optimizer="adadelta", lr=0):
+    """
+    Fonction pour entraîner le modèle.
 
-
-        name_code = "S" if mode == "RNNSearch" else "E"
-        name_code += "50" if seq_len == 50 else "30"
-        print("Let the train begin")
-        
-        self.device     = device
-        
-        model = model.to(device)
-        if load_model == "Nothing" : 
-            model.apply(self.Init_weights)
-            
-        else : 
-            state_dict_complet = torch.load(load_model, map_location=torch.device(device))
-            model.load_state_dict(state_dict_complet["model_state_dict"])
-
-
-        len_batches = len(train_data_loader)
-
-        if lr == 0 : lr = cfg.lr
-        
-        if chosed_optimizer == "adadelta": 
-            optimizer = torch.optim.Adadelta(model.parameters(), lr=lr, rho=0.95, eps=1e-06)
-        elif chosed_optimizer == "adam":
-            optimizer = torch.optim.Adam(model.parameters())
-
-        # if load_model != "Nothing" : 
-        #     optimizer.load_state_dict(state_dict_complet['optimizer_state_dict'])
-
-        criterion       = nn.CrossEntropyLoss(reduction="mean", ignore_index = 0) 
-
-        pbar            = pybar(range(cfg.epochs*len_batches), base_str="training")
-
-        train_losses    = []
-        val_losses      = []
-        
-        final_loss = val_loss = None
-
-        best_val_loss = float('inf')
-
-        best_model = None
-
-        if mode == "RNNSearch "    :  best_attention_weights = []
-        
-        for epoch in range(cfg.epochs):
-            
-            if mode == "RNNSearch "    : attention_weights=[]
-
-            model.train()
-
-            loss_for_one_epoch = []
-            n_batch = -1
-
-            for input_batch, output_batch  in train_data_loader :
-                n_batch += 1
-
-                input_batch, output_batch = input_batch.to(device), output_batch.to(device)
-
-                #output_batch_onehot = F.one_hot(torch.tensor(output_batch), num_classes=cfg.vocabulary_size).float().to(device)
-
-                optimizer.zero_grad()
-
-                if mode == "RNNEncDec" : output = model(input_batch)                  
-                else : output, attention_weights = model(input_batch)
-                    
-                output = output.reshape(-1, cfg.vocabulary_size)
-
-                # output_batch_onehot = output_batch_onehot.view(-1, cfg.vocabulary_size)
-
-                output_batch_onehot = output_batch.view(-1)
-
-                loss                        = criterion(output, output_batch_onehot)
-                loss.backward()
-
-                optimizer.step()
-
-                loss_for_one_epoch          += [loss.item()]
-                if nohup and n_batch % (len_batches//10) == 0 : print(f"{name_code} Epoch : {epoch}, batch : {n_batch}/{len_batches}, loss : {round(loss.item(),4)}")
-                if not self.quiet_mode  and not nohup : 
-                    pbar.set_description(description=f"{name_code} Epoch {epoch+1}, loss: {final_loss}, val: {val_loss}, Batch {n_batch+1}/{len_batches} loss : {round(loss.item(),4)}")
-                    pbar.__next__()
-
-
-                if n_batch+10%(len_batches//3) == 0 :
-                    val_loss                    = self.evaluate(model, val_data_loader, criterion, mode = mode)
+    Args:
+        model (nn.Module): Le modèle à entraîner.
+        train_data_loader (DataLoader): Le DataLoader pour les données d'entraînement.
+        val_data_loader (DataLoader): Le DataLoader pour les données de validation.
+        device (str): Le dispositif sur lequel exécuter l'entraînement (e.g., 'cuda' ou 'cpu').
+        nohup (bool, optional): Si True, l'entraînement est exécuté en mode nohup (pas de barre de progression). Par défaut False.
+        mode (str, optional): Le mode d'entraînement ('RNNEncDec' ou 'RNNSearch'). Par défaut "RNNEncDec".
+        seq_len (int, optional): La longueur de séquence utilisée. Par défaut cfg.sequence_length.
+        load_model (str, optional): Le chemin du modèle à charger avant l'entraînement. Par défaut "Nothing".
+        chosed_optimizer (str, optional): L'optimiseur à utiliser ('adadelta' ou 'adam'). Par défaut "adadelta".
+        lr (float, optional): Taux d'apprentissage. Si 0, utilise cfg.lr. Par défaut 0.
+    """
+    # ...
     
-                    if val_loss < best_val_loss :
-                        best_val_loss = val_loss
-                        best_model = model.state_dict()
-                        if mode == "RNNSearch "    : best_attention_weights=attention_weights
-        
-                        torch.save({'model_state_dict': best_model,
-                                    'optimizer_state_dict': optimizer.state_dict()}, 
-                                    f"{self.dir}/best_model.pth")
+    # Initialisation du code de nom en fonction du mode et de la longueur de séquence
+    name_code = "S" if mode == "RNNSearch" else "E"
+    name_code += "50" if seq_len == 50 else "30"
+    print("Let the training begin")
+    
+    self.device = device
+    
+    # Transférer le modèle sur le dispositif spécifié
+    model = model.to(device)
+    
+    # Initialiser les poids du modèle si aucun modèle n'est chargé
+    if load_model == "Nothing": 
+        model.apply(self.Init_weights)
+    else: 
+        state_dict_complet = torch.load(load_model, map_location=torch.device(device))
+        model.load_state_dict(state_dict_complet["model_state_dict"])
+
+    len_batches = len(train_data_loader)
+
+    # Définir le taux d'apprentissage si non spécifié
+    if lr == 0: 
+        lr = cfg.lr
+    
+    # Choisir l'optimiseur en fonction de l'option spécifiée
+    if chosed_optimizer == "adadelta": 
+        optimizer = torch.optim.Adadelta(model.parameters(), lr=lr, rho=0.95, eps=1e-06)
+    elif chosed_optimizer == "adam":
+        optimizer = torch.optim.Adam(model.parameters())
+
+    # Définir la fonction de perte (cross-entropy avec gestion du padding)
+    criterion = nn.CrossEntropyLoss(reduction="mean", ignore_index=0) 
+
+    # Initialiser la barre de progression
+    pbar = pybar(range(cfg.epochs * len_batches), base_str="training")
+
+    # Initialiser les listes pour stocker les pertes d'entraînement et de validation
+    train_losses = []
+    val_losses = []
+
+    final_loss = val_loss = None
+
+    best_val_loss = float('inf')
+
+    best_model = None
+
+    # Initialiser une liste pour stocker les poids d'attention (si le mode est RNNSearch)
+    if mode == "RNNSearch ":  
+        best_attention_weights = []
+
+    for epoch in range(cfg.epochs):
+
+        # Initialiser la liste des poids d'attention pour chaque lot d'entraînement (si le mode est RNNSearch)
+        if mode == "RNNSearch ": 
+            attention_weights = []
+
+        model.train()
+
+        loss_for_one_epoch = []
+        n_batch = -1
+
+        for input_batch, output_batch  in train_data_loader:
+            n_batch += 1
+
+            input_batch, output_batch = input_batch.to(device), output_batch.to(device)
+
+            # Initialiser les gradients à zéro
+            optimizer.zero_grad()
+
+            # Obtenir la sortie du modèle
+            if mode == "RNNEncDec": 
+                output = model(input_batch)
+            else: 
+                output, attention_weights = model(input_batch)
+
+            # Ajuster la forme de la sortie pour le calcul de la perte
+            output = output.reshape(-1, cfg.vocabulary_size)
+
+            # Ajuster la forme des cibles pour le calcul de la perte
+            output_batch_onehot = output_batch.view(-1)
+
+            # Calculer la perte
+            loss = criterion(output, output_batch_onehot)
+            loss.backward()
+
+            # Mettre à jour les poids du modèle
+            optimizer.step()
+
+            # Stocker la perte pour le lot actuel
+            loss_for_one_epoch += [loss.item()]
+
+            # Afficher les informations d'entraînement si en mode nohup
+            if nohup and n_batch % (len_batches // 10) == 0: 
+                print(f"{name_code} Epoch : {epoch}, batch : {n_batch}/{len_batches}, loss : {round(loss.item(), 4)}")
+
+            # Afficher les informations d'entraînement si en mode interactif
+            if not self.quiet_mode and not nohup: 
+                pbar.set_description(description=f"{name_code} Epoch {epoch+1}, loss: {final_loss}, val: {val_loss}, Batch {n_batch+1}/{len_batches} loss : {round(loss.item(), 4)}")
+                pbar.__next__()
+
+            # Évaluer le modèle sur les données de validation tous les 1/3 de lots
+            if n_batch + 10 % (len_batches // 3) == 0:
+                val_loss = self.evaluate(model, val_data_loader, criterion, mode=mode)
+
+                # Sauvegarder le meilleur modèle selon la perte de validation
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    best_model = model.state_dict()
+                    if mode == "RNNSearch ":  
+                        best_attention_weights = attention_weights
+
+                    torch.save({'model_state_dict': best_model,
+                                'optimizer_state_dict': optimizer.state_dict()}, 
+                                f"{self.dir}/best_model.pth")
                     model.train()
 
+        # Calculer la perte moyenne pour l'époque d'entraînement
+        final_loss = np.mean(loss_for_one_epoch)
 
+        # Calculer la perte moyenne pour l'époque de validation
+        val_loss = self.evaluate(model, val_data_loader, criterion, mode=mode)
 
-                
-            final_loss                  = np.mean(loss_for_one_epoch)
-            val_loss                    = self.evaluate(model, val_data_loader, criterion, mode = mode)
+        # Stocker les pertes d'entraînement et de validation
+        train_losses += [final_loss]
+        val_losses += [val_loss] 
 
-            train_losses                += [final_loss]
-            val_losses                  += [val_loss] 
+        # Arrondir les pertes pour l'affichage
+        final_loss = round(final_loss, 4)
+        val_loss = round(val_loss, 4)
 
-            final_loss                  = round(final_loss,4)
-            val_loss                    = round(val_loss,4)
+        # Sauvegarder les pertes sur disque
+        np.save(f"{self.dir}/loss.npy", np.array(train_losses))
+        np.save(f"{self.dir}/loss_val.npy", np.array(val_losses))
 
+        # Sauvegarder le modèle si la perte de validation est la meilleure jusqu'à présent
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_model = model.state_dict()
+            if mode == "RNNSearch ": 
+                best_attention_weights = attention_weights
 
-            np.save(f"{self.dir}/loss.npy", np.array(train_losses))
-            np.save(f"{self.dir}/loss_val.npy", np.array(val_losses))
-
-
-            if val_loss < best_val_loss :
-                best_val_loss = val_loss
-                best_model = model.state_dict()
-                if mode == "RNNSearch "    : best_attention_weights=attention_weights
-
-                torch.save({'model_state_dict': best_model,
-                            'optimizer_state_dict': optimizer.state_dict()}, 
-                            f"{self.dir}/best_model.pth")
-                torch.save(model, 
-                           f"{self.dir}/best_model_usable.pth")
-                
-            torch.save({'model_state_dict': model.state_dict(),
+            torch.save({'model_state_dict': best_model,
                         'optimizer_state_dict': optimizer.state_dict()}, 
-                        f"{self.dir}/last_model.pth")
+                        f"{self.dir}/best_model.pth")
+            torch.save(model, f"{self.dir}/best_model_usable.pth")
 
+        # Sauvegarder le modèle après chaque époque
+        torch.save({'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict()}, 
+                    f"{self.dir}/last_model.pth")
 
-            if nohup : print(f"Epoch {epoch + 1}/{cfg.epochs} : loss = {final_loss}, val_loss = {val_loss}")
-        
-        try :pbar.__next__()
-        except StopIteration : print("The training has ended")
+        # Afficher les informations d'entraînement si en mode nohup
+        if nohup: 
+            print(f"Epoch {epoch + 1}/{cfg.epochs} : loss = {final_loss}, val_loss = {val_loss}")
 
-    def evaluate(self, model, val_data_loader, criterion, mode = 'RNNEncDec'):
-        
-        model.eval()
-        
-        loss_for_one_epoch = []
+    try:
+        pbar.__next__()
+    except StopIteration:
+        print("The training has ended")
 
+def evaluate(self, model, val_data_loader, criterion, mode='RNNEncDec'):
+    """
+    Évalue le modèle sur l'ensemble de validation.
 
-        with torch.no_grad():
-            
-            for input_batch, output_batch in val_data_loader :
-                
-                input_batch, output_batch= input_batch.to(self.device), output_batch.to(self.device)
-                
-                #output_batch_onehot= F.one_hot(torch.tensor(output_batch), num_classes=cfg.vocabulary_size).float().to(self.device)
+    Paramètres :
+    - model : Le modèle PyTorch à évaluer.
+    - val_data_loader : DataLoader pour l'ensemble de validation.
+    - criterion : La fonction de perte utilisée pour l'évaluation.
+    - mode : Mode du modèle ('RNNEncDec' ou 'RNNSearch').
 
-                if mode == "RNNEncDec" : output  = model(input_batch)
-                else : output, attention_weights = model(input_batch)
-                    
-                output = output.reshape(-1, cfg.vocabulary_size)
+    Retourne :
+    - mean_loss : La perte moyenne sur l'ensemble de validation.
+    """
 
-                
-                # output_batch_onehot = output_batch_onehot.view(-1, cfg.vocabulary_size)
+    # Mettre le modèle en mode évaluation
+    model.eval()
 
-                output_batch_onehot = output_batch.view(-1)
+    # Liste pour stocker les pertes de chaque lot
+    loss_for_one_epoch = []
 
-                loss                        = criterion(output, output_batch_onehot)
+    # Désactiver le calcul du gradient pendant l'évaluation
+    with torch.no_grad():
 
-                loss_for_one_epoch          += [loss.item()]
+        # Itérer sur les lots de l'ensemble de validation
+        for input_batch, output_batch in val_data_loader:
 
-        return np.mean(loss_for_one_epoch)
+            # Déplacer les lots d'entrée et de sortie sur le périphérique spécifié
+            input_batch, output_batch = input_batch.to(self.device), output_batch.to(self.device)
+
+            # Obtenir les prédictions du modèle
+            if mode == 'RNNEncDec':
+                output = model(input_batch)
+            else:
+                output, attention_weights = model(input_batch)
+
+            # Remodeler la sortie pour calculer la perte
+            output = output.reshape(-1, cfg.vocabulary_size)
+
+            # Aplatir output_batch pour calculer la perte
+            output_batch_onehot = output_batch.view(-1)
+
+            # Calculer la perte pour le lot
+            loss = criterion(output, output_batch_onehot)
+
+            # Ajouter la perte à la liste
+            loss_for_one_epoch += [loss.item()]
+
+    # Calculer la perte moyenne sur tous les lots
+    mean_loss = np.mean(loss_for_one_epoch)
+
+    return mean_loss
